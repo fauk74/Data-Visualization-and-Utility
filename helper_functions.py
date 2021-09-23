3d2e3132abddf8fcf951a4083befac50963aa90f
fauk74 <65495422+fauk74@users.noreply.github.com>
2021-09-23 10:30:06 +0200

    Update helper_functions.py

--- helper_functions.py
+++ helper_functions.py
@@ -10,49 +10,7 @@ import random
 import os    
    
 def view_r_i(target_dir, target_class):
-  """
-  Reads in an image from filename, turns it into a tensor and reshapes into
-  (224, 224, 3).
-  Parameters
-  ----------
-  filename (str): string filename of target image
-  img_shape (int): size to resize target image to, default 224
-  scale (bool): whether to scale pixel values to range(0, 1), default True
-  """
-  #Setup target directory 
-  target_folder=target_dir+target_class
-  #Get a random path 
-  random_image=random.sample(os.listdir(target_folder),1)
-  #Read the image and plot
-  img=mpimg.imread(target_folder+"/" +random_image[0])
-  plt.imshow(img)
-  plt.title(target_class)
-  plt.axis("off")
-  print(f"Image shape: {img.shape}")
-
-
-def hello_world:
-  ####
-  """
-  Reads in an image from filename, turns it into a tensor and reshapes into
-  (224, 224, 3).
-  Parameters
-  ----------
-  filename (str): string filename of target image
-  img_shape (int): size to resize target image to, default 224
-  scale (bool): whether to scale pixel values to range(0, 1), default True
-  """
-  # Read in the image
-  img = tf.io.read_file(filename)
-  # Decode it into a tensor
-  img = tf.image.decode_jpeg(img)
-  # Resize the image
-  img = tf.image.resize(img, [img_shape, img_shape])
-  if scale:
-    # Rescale the image (get all values between 0 and 1)
-    return img/255.
-  else:
-    return img
+  print("Attempt")
 
 
 
