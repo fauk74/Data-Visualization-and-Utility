55faba112c5e6dd8b640046ee8ca8adf9cecce50
fauk74 <65495422+fauk74@users.noreply.github.com>
2021-09-23 10:28:28 +0200

    Update helper_functions.py

--- helper_functions.py
+++ helper_functions.py
@@ -10,7 +10,7 @@ import random
 import os    
    
 def view_r_i(target_dir, target_class):
-     """
+  """
   Reads in an image from filename, turns it into a tensor and reshapes into
   (224, 224, 3).
   Parameters
@@ -33,8 +33,26 @@ def view_r_i(target_dir, target_class):
 
 def hello_world:
   ####
-   print("hello world")
-
+  """
+  Reads in an image from filename, turns it into a tensor and reshapes into
+  (224, 224, 3).
+  Parameters
+  ----------
+  filename (str): string filename of target image
+  img_shape (int): size to resize target image to, default 224
+  scale (bool): whether to scale pixel values to range(0, 1), default True
+  """
+  # Read in the image
+  img = tf.io.read_file(filename)
+  # Decode it into a tensor
+  img = tf.image.decode_jpeg(img)
+  # Resize the image
+  img = tf.image.resize(img, [img_shape, img_shape])
+  if scale:
+    # Rescale the image (get all values between 0 and 1)
+    return img/255.
+  else:
+    return img
 
 
 
